342ed5c7713817b8f9f62db35608dd6b90219772
ghran-dz <ghrandiaz@gmail.com>
2025-04-09 09:34:46 +0700

    Update indexxxx.php
    Google Tagging

--- indexxxx.php
+++ indexxxx.php
@@ -2,6 +2,16 @@
 <html lang="en">
 
 <head>
+        <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZNYRYPDTTY"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-ZNYRYPDTTY');
+    </script>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HAKA BYD</title>
